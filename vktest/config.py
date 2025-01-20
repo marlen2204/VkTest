66ec79e2b253eb1505b5ed033a865d33d34fa440
Marina Slobozhaninova <slobozhaninovaw@gmail.com>
2025-01-20 13:44:49 +0000

    add support mac and windows

--- vktest/config.py
+++ vktest/config.py
@@ -1,9 +1,19 @@
+import platform
+
+# Определение подходящего ioengine в зависимости от ОС
+if platform.system() == "Darwin":  # macOS
+    IOENGINE = "posixaio"
+elif platform.system() == "Windows":
+    IOENGINE = "windowsaio"
+else:  # Linux
+    IOENGINE = "libaio"
+
 # Глубина очереди (iodepth)
 IODEPTH_VALUES = [1, 2, 4, 8, 16, 32, 64, 128, 256]
 
 # Базовые параметры fio
 FIO_BASE_PARAMS = [
-    "--ioengine=libaio",
+    "--ioengine={IOENGINE}",
     "--direct=1",
     "--bs=4k",
     "--size=1G",
